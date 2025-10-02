37508373f5ef9a47d0a1c0d8111e5ce3ad73722e
Andrew Chung <26929776+AndyHWChung@users.noreply.github.com>
2025-10-02 15:10:13 +0100

    [AC][OPS] library updates (#30)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,7 +5,7 @@ object Dependencies {
   private val gatlingVersion = "3.6.1"
 
   val test = Seq(
-    "com.typesafe"          % "config"                    % "1.4.4"        % Test,
+    "com.typesafe"          % "config"                    % "1.4.5"        % Test,
     "uk.gov.hmrc"          %% "performance-test-runner"   % "6.2.0"        % Test
   )
 }
