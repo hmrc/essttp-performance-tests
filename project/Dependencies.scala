8f3810d269d77862d803c5256132813e836de15b
AndyHWChung <26929776+AndyHWChung@users.noreply.github.com>
2025-10-09 14:08:51 +0100

    [AC][OPS-14168] update runner; fix tests

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,6 +6,6 @@ object Dependencies {
 
   val test = Seq(
     "com.typesafe"          % "config"                    % "1.4.5"        % Test,
-    "uk.gov.hmrc"          %% "performance-test-runner"   % "6.2.0"        % Test
+    "uk.gov.hmrc"          %% "performance-test-runner"   % "6.3.0"        % Test
   )
 }
